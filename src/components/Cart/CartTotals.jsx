e9e1ea1beff0f5cf2b09a63d6be5ed014cee2704
Ayodejioladimeji <layobright11@gmail.com>
2021-03-05 13:15:12 -0800

    added updates

--- src/components/Cart/CartTotals.jsx
+++ src/components/Cart/CartTotals.jsx
@@ -1,23 +1,23 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import PayPalButton from './PayPalButton'
 
-const CartTotals = ({value,history}) => {
-    const {cartSubtotal, cartTotal, clearCart} = value;
+const CartTotals = ({ value, history }) => {
+    const { cartSubtotal, cartTotal, clearCart } = value;
     return (
         <>
             <div className="container">
                 <div class="row">
                     <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
-                        <Link t0="/">
-                            <button className="btn btn-outline-danger text-uppercase mb-3 px-5" type="button" onClick={()=>clearCart()}>clear cart</button>
+                        <Link to="/">
+                            <button className="btn btn-outline-danger text-uppercase mb-3 px-5" type="button" onClick={() => clearCart()}>clear cart</button>
                         </Link>
 
                         <h5>
                             <span className="text-title">subtotal :</span>
                             ₦ <strong>{cartSubtotal.toLocaleString()}</strong>
                         </h5>
-                        
+
                         {/* <h5>
                             <span className="text-title">tax :</span>
                             <strong>{cartTax}</strong>
@@ -26,8 +26,8 @@ const CartTotals = ({value,history}) => {
                         <h5>
                             <span className="text-title">total : </span>
                             ₦ <strong>{cartTotal.toLocaleString()}</strong>
-                        </h5> 
-                        <PayPalButton total={cartTotal} clearCart={clearCart} history={history}/>
+                        </h5>
+                        <PayPalButton total={cartTotal} clearCart={clearCart} history={history} />
                     </div>
                 </div>
             </div>
